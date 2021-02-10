1be41e86ef0651d16cdf3640150bacc30e2e9129
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-02-10 13:52:21 +0000

    Add files via upload

---
+++ AirlineReservation.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+
+class AirlineReservation
+{
+        
+    bool[] firstClass = new bool[5];
+    bool[] secondClass = new bool[5];
+    public int travelClass;
+    public int seatsTwo;
+    public int seats;
+
+    public void SetTravelChoice(int airlineReservationTravelClass)
+    {
+        travelClass = airlineReservationTravelClass;
+    }
+
+    public int GetTravelchoice()
+    {
+        return travelClass;
+    }
+
+    public void FirstClassBooking()
+    {
+        for (seats = 0; seats < firstClass.Length; seats++)
+        {
+            if (firstClass[4] == true && secondClass[4] == true)
+            {
+                Console.WriteLine("\n\n\t\t!!! Sorry, this flight is full !!!");
+                travelClass = 0;
+                break;
+            }
+            else if (firstClass[4] == true)
+            {
+                Console.WriteLine("\nThe first class has no more vacant seat, would you consider seat on the 2nd class? y or n: ");
+                string decision = Console.ReadLine();
+                if (decision == "y")
+                {
+                    Console.WriteLine("You were transfered to the second class.");
+                    SecondClassBooking();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nNext flight leaves in 3 hours.");
+                    break;
+                }
+            }
+            else if (firstClass[seats] == true)
+            {
+                continue;
+            }
+            else if (firstClass[seats] != true)
+            {
+                firstClass[seats] = true;
+                Console.WriteLine("Your reservation was made on the first class");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Your answer does not specify a valid choice.");
+                break;
+            }
+        }
+        
+    }
+
+
+    public void SecondClassBooking()
+    {
+        for (seatsTwo = 0; seatsTwo < secondClass.Length; seatsTwo++)
+        {
+            if (firstClass[4] == true && secondClass[4] == true)
+            {
+                Console.WriteLine("\n\n\t\t!!! Sorry, this flight is full !!!");
+                travelClass = 0;
+                break;
+            }
+            else if (secondClass[4] == true)
+            {
+                Console.WriteLine("\nThe second class has no more vacant seat, would you consider seat on the 2nd class? y or n: ");
+                string decision = Console.ReadLine();
+                if (decision == "y")
+                {
+                    Console.WriteLine("You were transfered to the first class.");
+                    FirstClassBooking();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nNext flight leaves in 3 hours.");
+                    break;
+                }
+            }
+            else if (secondClass[seatsTwo] == true)
+            {
+                continue;
+            }
+            else if (secondClass[seatsTwo] != true)
+            {
+                secondClass[seatsTwo] = true;
+                Console.WriteLine("Your reservation was made on the second class.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("\nYour answer does not specify a valid choice.");
+                break;
+            }
+        }
+    }
+
+    public void DisplayFirstClass()
+    {
+        for (int i = 0; i < firstClass.Length; i++)
+        {
+            if (firstClass[i] == true)
+            {
+                Console.WriteLine($"Seat {i + 1} is occupied");
+            }
+            else
+                Console.WriteLine($"Seat {i + 1} is vacant");   
+        }
+        Console.WriteLine("");
+    }
+
+    public void DisplaySecondClass()
+    {
+        for (int j = 0; j < secondClass.Length; j++)
+        {
+            if (secondClass[j] == true)
+            {
+                Console.WriteLine($"Seat {j + 6} is occupied");
+            }
+            else
+                Console.WriteLine($"Seat {j + 6} is vacant");
+        }
+        Console.WriteLine(" ");
+    }
+
+}
+
+
+
---
+++ AirlineReservationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+class AirlineReservationTest
+{
+    static void Main()
+    {
+        AirlineReservation airlineReservation = new AirlineReservation();
+
+
+        Console.WriteLine("\t<<<<<<<<<<Welcome on the New Airline's reservation system>>>>>>>>>>\n\n");
+
+        Console.WriteLine("\n\nSeat availability:\n\n");
+        
+        airlineReservation.DisplayFirstClass();
+
+        airlineReservation.DisplaySecondClass();
+
+
+        do
+        {
+            Console.Write("\nPlease indicate what class would you like to travel on(1 or 2) or 0 to exit: ");
+            int airlineReservationtheTravelChoice = int.Parse(Console.ReadLine());
+            airlineReservation.SetTravelChoice(airlineReservationtheTravelChoice);
+
+            if (airlineReservationtheTravelChoice == 1)
+                airlineReservation.FirstClassBooking();
+            else if (airlineReservationtheTravelChoice == 2)
+                airlineReservation.SecondClassBooking();
+            else if (airlineReservationtheTravelChoice == 0)
+                break;
+            else
+            {
+                Console.WriteLine("Your answer does not specify a valid choice.");
+                continue;
+            }
+
+        } while (airlineReservation.GetTravelchoice() != 0);
+
+        Console.WriteLine("\n\nSeat availability: \n\n");
+
+        airlineReservation.DisplayFirstClass();
+
+        airlineReservation.DisplaySecondClass();
+
+        Console.WriteLine("\t\t\t<<<<<<<< Thank you for choosing to fly with us >>>>>>>>");
+
+    }
+
+}
